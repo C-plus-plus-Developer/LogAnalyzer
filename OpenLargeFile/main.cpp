e9a5dd894b0717a9a30ecf3c74525a3893c3aab0
Михаил <mihailbracin@gmail.com>
2026-03-25 19:52:49 +0300

    Update main.cpp

--- OpenLargeFile/main.cpp
+++ OpenLargeFile/main.cpp
@@ -5,8 +5,6 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    //Эти штуки нужны чтобы QT создавал файл с историей, например, чтобы не вводить каждый раз
-    //путь и имя файла
     QCoreApplication::setOrganizationName("SoftWare");
     QCoreApplication::setApplicationName("LogViewer");
 
